b535924fab37b0c0c1a29a88aee842fc51fdf971
karthik kolluru <karthikkolluru123@gmail.com>
2024-04-29 17:13:15 +0530

    Updated: User model

--- users/models.py
+++ users/models.py
@@ -20,6 +20,33 @@ class Department(BaseModel):
 
 class User(AbstractUser):
     name = models.CharField(null=False, max_length=32)
+    #username
+    #password
+    designation = models.CharField('''choices''',max_length=32,db_index=True)
+    employee_id = models.IntegerField(null=False,db_index=True)
+    department = models.ForeignKey(Department,on_delete=models.PROTECT)
+    #date_joined
+    picture = models.ImageField(upload_to="profile", null=True)
+    mobile = models.IntegerField(max_length=16, null=False,unique=True)
+    email = models.EmailField(unique=True,null=False,db_index=True)
+    dob = models.DateField(null=True)
+    door_no = models.CharField(max_length=32)
+    street = models.CharField(max_length=64)
+    #locality
+    #city
+    gender = models.CharField('''choices''',null=False)
+    reporting_to = models.ForeignKey('self',on_delete=models.PROTECT)
+    passport_no = models.CharField(max_length=16, null=True,unique=True)
+    nationality = models.CharField(max_length=32,null=True)
+    marital_status = models.CharField('''choices''',max_length=16,null=True)
+    no_of_children = models.IntegerField(null = True)
+    #groups
+    blood_group = models.CharField('''choices''',max_length=12, null=True)
+    social_id_no = models.CharField(max_length=16,unique=True,null=True)
+
+
+
+
 
 
 
@@ -28,8 +55,8 @@ class UserBankdetails(BaseModel):
     user_id = models.ForeignKey(User, on_delete = models.PROTECT, null =False, db_index=True)
     bank_name = models.CharField(max_length=32, null = False)
     ifsc = models.CharField(max_length=16,null = False)
-    base_branch = models.CharField(max_length=32,default = None)
-    pan_card = models.CharField(max_length=16, default= None)
+    base_branch = models.CharField(max_length=32,null=True)
+    pan_card = models.CharField(max_length=16, null=True)
     is_company_issued = models.BooleanField(default=False, db_index = True)
 
 
@@ -38,31 +65,33 @@ class FamilyMembers(BaseModel):
 
     user_id = models.ForeignKey(User, on_delete=models.PROTECT, db_index=True)
     name = models.CharField(max_length=32, null=False)
-    relationship = models.CharField(max_length=32, default = None)
+    relationship = models.CharField(max_length=32, null=True, default = None)
     mobile = models.CharField(max_length=12, null = False)
-    email = models.EmailField(default = None)
-    dob = models.DateTimeField(default = None)
+    email = models.EmailField(null = True)
+    dob = models.DateTimeField(null=True)
     is_emergency_contact = models.BooleanField(default = True)
 
 
 class EducationalQualifications(BaseModel):
+
     user_id = models.ForeignKey(User, on_delete=models.PROTECT,db_index=True)
     start_date = models.DateField(null = False)
     end_date = models.DateField(default = None)
     institute_name = models.CharField(max_length = 128, default=None)
     percentage = models.FloatField(null = False)
     course = models.CharField(max_length=64, null = False)
-    level = models.CharField(default = None)
+    level = models.CharField(null=True, default = None)
 
 
 class UserExperience(BaseModel):
+    
     user_id = models.ForeignKey(User, on_delete= models.PROTECT, db_index = True)
     title = models.CharField(max_length=64, null = False)
     place_of_work = models.CharField(max_length=50, null = False)
     start_datetime = models.DateField(default= None, db_index=True)
     end_datetime = models.DateField(null=False, db_index=True)
-    skills = models.CharField(max_length=512, default= None)
-    achievements = models.CharField(max_length= 512, default = None)
+    skills = models.CharField(max_length=512, default= None, null=True)
+    achievements = models.CharField(max_length= 512, default = None,null=True)
 
 
 class Country(BaseModel):
@@ -87,9 +116,8 @@ class UserGroup(BaseModel):
     name = models.CharField(max_length=32, unique=True,db_index=True)
 
     def save(self,*args,**kwargs):
-        '''
-        get or create group with name
-        '''
+
+       # get or create group with name, returns tuple(object,true/false)
         group, created = Group.objects.get_or_create(name = self.name)
         return super().save(*args,**kwargs)
     
