173f75991e1b51e3f7c0a02791af7f2430dfa4eb
karthik kolluru <karthikkolluru123@gmail.com>
2024-04-30 19:06:30 +0530

    Added: New fields to User Model

--- users/models.py
+++ users/models.py
@@ -18,29 +18,47 @@ class Department(BaseModel):
   # department_head = models.IntegerField(default = null)
 
 
+
+class Country(BaseModel):
+    name = models.CharField(unique=True, null = False, db_index=True)
+
+
+class State(BaseModel):
+    name = models.CharField(max_length=32, null = False)
+    country =  models.CharField(Country,on_delete=models.PROTECT, null=False)
+
+
+class City(BaseModel):
+    name = models.CharField(max_length=32, null = False)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    state = models.ForeignKey(State, on_delete = models.PROTECT)
+    latitude = models.CharField(null=True)
+    longitude = models.CharField(null=True)
+
+
 class User(AbstractUser):
     name = models.CharField(null=False, max_length=32)
-    #username
-    #password
+    # username
+    # password
     designation = models.CharField('''choices''',max_length=32,db_index=True)
-    employee_id = models.IntegerField(null=False,db_index=True)
+    employee_id = models.CharField(null=False,unique=True,db_index=True)
     department = models.ForeignKey(Department,on_delete=models.PROTECT)
-    #date_joined
+    # date_joined
     picture = models.ImageField(upload_to="profile", null=True)
     mobile = models.IntegerField(max_length=16, null=False,unique=True)
     email = models.EmailField(unique=True,null=False,db_index=True)
     dob = models.DateField(null=True)
     door_no = models.CharField(max_length=32)
     street = models.CharField(max_length=64)
-    #locality
-    #city
+    locality = models.CharField(max_length=32)
+    city = models.ForeignKey(City, on_delete= models.PROTECT)
     gender = models.CharField('''choices''',null=False)
     reporting_to = models.ForeignKey('self',on_delete=models.PROTECT)
     passport_no = models.CharField(max_length=16, null=True,unique=True)
     nationality = models.CharField(max_length=32,null=True)
     marital_status = models.CharField('''choices''',max_length=16,null=True)
     no_of_children = models.IntegerField(null = True)
-    #groups
+    # groups
     blood_group = models.CharField('''choices''',max_length=12, null=True)
     social_id_no = models.CharField(max_length=16,unique=True,null=True)
 
@@ -52,7 +70,7 @@ class User(AbstractUser):
 
 class UserBankdetails(BaseModel):
     
-    user_id = models.ForeignKey(User, on_delete = models.PROTECT, null =False, db_index=True)
+    user = models.ForeignKey(User, on_delete = models.PROTECT, null =False, db_index=True)
     bank_name = models.CharField(max_length=32, null = False)
     ifsc = models.CharField(max_length=16,null = False)
     base_branch = models.CharField(max_length=32,null=True)
@@ -63,7 +81,7 @@ class UserBankdetails(BaseModel):
 
 class FamilyMembers(BaseModel):
 
-    user_id = models.ForeignKey(User, on_delete=models.PROTECT, db_index=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, db_index=True)
     name = models.CharField(max_length=32, null=False)
     relationship = models.CharField(max_length=32, null=True, default = None)
     mobile = models.CharField(max_length=12, null = False)
@@ -72,9 +90,9 @@ class FamilyMembers(BaseModel):
     is_emergency_contact = models.BooleanField(default = True)
 
 
-class EducationalQualifications(BaseModel):
+class EducationQualifications(BaseModel):
 
-    user_id = models.ForeignKey(User, on_delete=models.PROTECT,db_index=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT,db_index=True)
     start_date = models.DateField(null = False)
     end_date = models.DateField(default = None)
     institute_name = models.CharField(max_length = 128, default=None)
@@ -85,30 +103,14 @@ class EducationalQualifications(BaseModel):
 
 class UserExperience(BaseModel):
     
-    user_id = models.ForeignKey(User, on_delete= models.PROTECT, db_index = True)
+    user = models.ForeignKey(User, on_delete= models.PROTECT, db_index = True)
     title = models.CharField(max_length=64, null = False)
     place_of_work = models.CharField(max_length=50, null = False)
     start_datetime = models.DateField(default= None, db_index=True)
     end_datetime = models.DateField(null=False, db_index=True)
-    skills = models.CharField(max_length=512, default= None, null=True)
-    achievements = models.CharField(max_length= 512, default = None,null=True)
+    skills = models.CharField(max_length=512, null=True)
+    achievements = models.CharField(max_length= 512,null=True)
 
-
-class Country(BaseModel):
-    name = models.CharField(unique=True, null = False, db_index=True)
-
-
-class State(BaseModel):
-    name = models.CharField(max_length=32, null = False)
-    country_id =  models.IntegerField(default=None)
-
-
-class City(BaseModel):
-    name = models.CharField(max_length=32, null = False)
-    country_id = models.ForeignKey(Country, on_delete=models.PROTECT)
-    state_id = models.ForeignKey(State, on_delete = models.PROTECT)
-    latitude = models.CharField(null=True)
-    longitude = models.CharField(null=True)
 
 
 
@@ -117,7 +119,7 @@ class UserGroup(BaseModel):
 
     def save(self,*args,**kwargs):
 
-       # get or create group with name, returns tuple(object,true/false)
+       # get or create group with name, returns tuple(Group_object,true/false)
         group, created = Group.objects.get_or_create(name = self.name)
         return super().save(*args,**kwargs)
     
